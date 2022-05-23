87e0e32248c3732a579549c74b811f13eae91c92
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2022-05-23 11:37:11 -0400

    refactor: utils (#20)
    Rewrote utils library in go

---
+++ utils/utils.go
@@ -0,0 +1,172 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+// handleError provided a generic implenation of colly.OnError
+func handleError(c *colly.Collector) {
+	c.OnError(func(r *colly.Response, err error) { // Set error handler
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+}
+
+// GetPrice get the price of an product
+func GetPrice(c *colly.Collector, price *float64) {
+	c.OnHTML(".ourprice", func(e *colly.HTMLElement) {
+		stringPrice := e.Text[2:]
+
+		value, err := strconv.ParseFloat(stringPrice, 32)
+		if err != nil {
+			log.Fatalf("Can not convert price %s to interger", e.Text)
+		}
+		*price = value
+	})
+
+	handleError(c)
+}
+
+// GetDiscountPercentage returns the discount percentage of the product passed in
+func GetDiscountPercentage(c *colly.Collector, discountPercentage *float64) {
+	c.OnHTML(".yousave", func(e *colly.HTMLElement) {
+		discountPercentageString := strings.TrimSpace(e.Text)
+
+		openBracket := strings.Index(discountPercentageString, "(")
+		percentSign := strings.Index(discountPercentageString, "%")
+		discountPercentageString = discountPercentageString[openBracket+1 : percentSign]
+
+		value, err := strconv.ParseFloat(discountPercentageString, 32)
+		if err != nil {
+			log.Fatalf("Can not convert price %s to interger", e.Text)
+		}
+
+		*discountPercentage = value
+	})
+
+	handleError(c)
+}
+
+// GetDiscountedPrice return the discount price of the product
+func GetDiscountedPrice(c *colly.Collector, price *float64) {
+
+	c.OnHTML(".yousave", func(e *colly.HTMLElement) {
+		discountPriceString := strings.TrimSpace(e.Text)
+
+		discountPriceString = strings.Replace(discountPriceString, "$", "", 1)
+		firstSpace := strings.Index(discountPriceString, " ")
+		// get string up to first space
+		discountPriceString = discountPriceString[:firstSpace]
+
+		value, err := strconv.ParseFloat(discountPriceString, 32)
+		if err != nil {
+			log.Fatalf("Can not convert price %s to interger", e.Text)
+		}
+
+		*price = value
+	})
+
+	handleError(c)
+}
+
+// GetStarRating return the number of starts this product has
+func GetStarRating(c *colly.Collector, rating *int64) {
+
+	c.OnHTML("#review_summary", func(e *colly.HTMLElement) {
+		ratingLink := e.ChildAttr("img", "src")
+		lastSlash := strings.LastIndex(ratingLink, "/")
+		stringRating, err := strconv.ParseInt(ratingLink[lastSlash+1:lastSlash+2], 0, 0)
+		if err != nil {
+			log.Fatalf("Unable to convert rating %v to int", ratingLink[lastSlash+1:])
+		}
+		*rating = stringRating
+	})
+	handleError(c)
+}
+
+// GetTitle gets the title of a product
+func GetTitle(c *colly.Collector, title *string) {
+	c.OnHTML("#product_name", func(e *colly.HTMLElement) {
+		*title = e.ChildText("h1")
+	})
+	handleError(c)
+}
+
+// GetDescription get the description of a product
+func GetDescription(c *colly.Collector, description *string) {
+	c.OnHTML(".product_subsection", func(e *colly.HTMLElement) {
+		des := e.ChildText("p")
+		if des != "" {
+			*description = des
+		}
+	})
+	handleError(c)
+}
+
+// addNotInterested add a product to the not interested list. Return true if product was successfully written to file. False other wise
+func AddNotInterested(productTitle string) bool {
+	// open a csv file
+	file, err := os.OpenFile("not_interested.csv", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("Unable to open file: %v", err)
+		return false
+	}
+	defer file.Close()
+
+	// ask for user confirmation before writing to file
+	fmt.Printf("Are you sure you want to add %s to the not interested list? (y/n)", productTitle)
+	var answer string
+	fmt.Scanln(&answer)
+
+	if answer == "y" {
+		// write the product title to the file if it is not already in the file
+		if _, err := file.WriteString(productTitle + "\n"); err != nil {
+			fmt.Printf("Unable to write to file: %v", err)
+		}
+		return true
+	}
+	return false
+}
+
+// IfInterested check if the product passed in is interesting.
+// Return true if product is interesting. False otherwise
+func IfInterested(title string) bool {
+	// open a csv file
+	file, err := os.OpenFile("../not_interested_products.csv", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("Unable to open file: %v", err)
+		return false
+	}
+	defer file.Close()
+
+	// read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// if the product title is in the file, product is not intersting
+		if title == scanner.Text() {
+			return false
+		}
+	}
+	return true
+}
+
+func AddNotInterestedProduct(productTitle string) {
+	// open a csv file
+	file, err := os.OpenFile("../not_interested_products.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("Unable to open file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	// write the product title to end of the file if it is not already in the file
+	if _, err := file.WriteString(productTitle + "\n"); err != nil {
+		fmt.Printf("Unable to write to file: %v", err)
+	}
+}
---
+++ utils/utils_test.go
@@ -0,0 +1,121 @@
+package utils_test
+
+import (
+	"log"
+	"os/exec"
+	"testing"
+
+	"github.com/gocolly/colly"
+	utils "github.com/shawnyu5/penguin-utils"
+)
+
+var c *colly.Collector
+
+// handleError provided a generic implenation of colly.OnError
+func handleError(c *colly.Collector) {
+	c.OnError(func(r *colly.Response, err error) { // Set error handler
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+}
+
+// beforeEach call before each test case
+func beforeEach() {
+	c = colly.NewCollector(
+		colly.AllowedDomains("www.penguinmagic.com", "www.penguinmagic.com/p/3901"),
+	)
+}
+
+// visit visit web address to start scraping
+func visit(c *colly.Collector) {
+	c.Visit("https://www.penguinmagic.com/p/3901")
+
+}
+
+func TestGetDiscountPercentage(t *testing.T) {
+	beforeEach()
+	var output float64
+	utils.GetDiscountPercentage(c, &output)
+
+	visit(c)
+
+	if output != 50 {
+		t.Errorf("Expected %v, got %v", 50, output)
+	}
+}
+
+func TestGetStarRating(t *testing.T) {
+	beforeEach()
+	var output int64
+	utils.GetStarRating(c, &output)
+
+	visit(c)
+
+	if output != 4 {
+		t.Fatalf("Expected %d, got %d", 4, output)
+	}
+}
+
+func TestGetPrice(t *testing.T) {
+	beforeEach()
+	var output float64
+	utils.GetPrice(c, &output)
+	handleError(c)
+
+	visit(c)
+
+	if output != 4.949999809265137 {
+		t.Errorf("Expected %v, got %v", 4.949999809265137, output)
+	}
+}
+
+func TestGetDiscountPrice(t *testing.T) {
+	beforeEach()
+	var output float64
+	utils.GetDiscountedPrice(c, &output)
+	expected := 5.050000190734863
+	handleError(c)
+
+	visit(c)
+
+	if output != expected {
+		t.Errorf("Got %f. Expected %f", output, expected)
+	}
+}
+
+func TestGetDescription(t *testing.T) {
+	beforeEach()
+	handleError(c)
+	var output string
+	expected := "Nick Diffatte has created a wonderful pocket illusion you can use ANYTIME you're going to pay for something.\n\nPull out a slip of Monopoly Money, and when the clerk informs you they don't accept that form of currency, visually change it in FULL VIEW."
+
+	utils.GetDescription(c, &output)
+
+	visit(c)
+
+	if output != expected {
+		t.Fatalf("Expected: %s, got %s", expected, output)
+	}
+}
+
+func TestIfIntersting(t *testing.T) {
+
+	interesting := utils.IfInterested("jfdslfj")
+	notInteresting := utils.IfInterested("Code Red by Cody Nottingham (DVD & Download)")
+	if !interesting {
+		t.Fatalf("Expected true, got %v", interesting)
+	}
+
+	if notInteresting {
+		t.Fatalf("Expected false, got %v", notInteresting)
+	}
+}
+
+func TestAddNotInterestedProduct(t *testing.T) {
+	utils.AddNotInterestedProduct("jfdslfj")
+
+	// delete test string from file again
+	_, err := exec.Command("sed", "-i", "/jfdslfj/d", "../not_interested_products.csv").Output()
+	if err != nil {
+		panic(err)
+	}
+}
