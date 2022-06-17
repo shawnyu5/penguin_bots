bf0349189ca7aa428c7941d4f38a4dd3eb2f3d20
shawn yu <shawn.yu5@hotmail.com>
2022-06-17 16:51:47 -0400

    feat: type def `process.env.*`

---
+++ bot/enviroment.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+   interface ProcessEnv {
+      TOKEN: string;
+      GUILDID: string;
+      CLIENTID: string;
+      MONGOOSE_KEY: string;
+      API_ADDRESS: string;
+   }
+}
