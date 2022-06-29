6332c00e1bacc197cebc2a1d867385726d4b5e25
shawn yu <shawn.yu5@hotmail.com>
2022-06-29 15:22:01 -0400

    chore: added comments in config.ts

--- bot/src/types/config.ts
+++ bot/src/types/config.ts
@@ -1,9 +1,13 @@
 export interface IConfig {
+   // discord bot token
    TOKEN: string;
    GUILDID: string;
+   // discord bot client id
    CLIENTID: string;
    COIN_PRODUCT_ALERT_USERS: Array<string>;
+   // mongodb connection string
    MONGOOSE_KEY: string;
+   // api address for coin product
    API_ADDRESS: string;
    DEVELOPMENT: "true" | "false";
    LOG_LEVEL: "info" | "debug" | "error" | "warn" | "trace";
