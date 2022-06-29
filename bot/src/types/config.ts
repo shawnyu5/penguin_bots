7152f2620875b0cb98257646b02d791bad8030a8
shawn yu <shawn.yu5@hotmail.com>
2022-06-29 15:00:19 -0400

    feat: updated config types

--- bot/src/types/config.ts
+++ bot/src/types/config.ts
@@ -1,3 +1,10 @@
 export interface IConfig {
-   coin_product_alert_users: Array<string>;
+   TOKEN: string;
+   GUILDID: string;
+   CLIENTID: string;
+   coin_product_alert_users: ["652511543845453855"];
+   MONGOOSE_KEY: string;
+   API_ADDRESS: string;
+   DEVELOPMENT: true;
+   LOG_LEVEL: "info" | "debug" | "error" | "warn" | "trace";
 }
