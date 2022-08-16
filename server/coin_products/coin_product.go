3fe8318aab44bc60d749fa4ecc51c824fea6d612
shawn yu <shawn.yu5@hotmail.com>
2022-08-16 00:03:31 -0400

    fix: tests using new interface

--- server/coin_products/coin_product.go
+++ server/coin_products/coin_product.go
@@ -36,59 +36,55 @@ type CoinProduct struct {
 }
 
 // var PRODUCT_INFO_FILE string
-var storage *cache.Cache
+// var storage *cache.Cache
+
+func (CoinProductServiceImpl) makeDB() *bitcask.Bitcask {
+	b, err := bitcask.Open("./db")
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
 
 // Check gets the product from the url passed in, and checks if it's a coin product, and if it is an interesting product.
-// NOTE this function is not responsible for checking if the product has changed.
-// Returns a CoinProduct struct
-func Check(url string) CoinProduct {
-	homeDir, _ := os.UserHomeDir()
-	storage = cache.New(cache.NoExpiration, 30*time.Minute)
-	utils.SetFilePath(homeDir + "/python/penguin_bots/not_interested_products.csv")
+// sets the product.IsValid field to true if it is a coin product. False otherwise. And the product.Reason field
+func (CoinProductServiceImpl) Check(product *CoinProduct) {
+	// homeDir, _ := os.UserHomeDir()
+	// storage = cache.New(cache.NoExpiration, 30*time.Minute)
+	// utils.SetFilePath(homeDir + "/python/penguin_bots/not_interested_products.csv")
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.penguinmagic.com",
-			"www.penguinmagic.com/p/17235", "www.penguinmagic.com/openbox",
-			"www.penguinmagic.com/p/3901", url),
-	)
+	// c := colly.NewCollector(
+	// colly.AllowedDomains("www.penguinmagic.com",
+	// "www.penguinmagic.com/p/17235", "www.penguinmagic.com/openbox",
+	// "www.penguinmagic.com/p/3901", url),
+	// )
 
-	product := CoinProduct{}
+	// var p CoinProductService
+	// p = CoinProductServiceImpl{}
 
-	getProductInfo(c, &product, url)
+	// product := CoinProduct{}
+
+	// p.getProductInfo(c, &product, url)
 
 	// if product is empty, then openbox is down right now
 	if product.Title == "" {
 		product.IsValid = false
 		product.Reason = "Openbox is down currently..."
-		log.Println(product.Reason)
 	} else if !utils.IfInterested(product.Title) {
 		product.IsValid = false
 		product.Reason = fmt.Sprintf("Product %s is not interested", product.Title)
-		log.Println(product.Reason)
-		// NOTE: it should be up to the caller to determine if the product has changed from the last call of this function
-		// } else if !hasProductChanged(&product) {
-		// product.IsValid = false
-		// product.Reason = fmt.Sprintf("Product *%s* has not changed", product.Title)
-		// log.Println(product.Reason)
-	} else if !isCoinProduct(&product) {
+	} else if !isCoinProduct(product) {
 		product.IsValid = false
 		product.Reason = fmt.Sprintf("Product *%s* is not a coin product", product.Title)
-		log.Println(product.Reason)
 	} else {
 		product.IsValid = true
 		product.Reason = "Product is a coin product"
 	}
 
-	// parse into json
-	// parsedJson, err := json.MarshalIndent(product, "", "  ")
-	// if err != nil {
-	// panic(err)
-	// }
-	return product
 }
 
-// getProductInfo get the product currently on penguin open box
-func getProductInfo(c *colly.Collector, product *CoinProduct, url string) {
+// getProductInfo get the product currently on penguin open box. Stores the product info in product struct passed in
+func (CoinProductServiceImpl) getProductInfo(c *colly.Collector, product *CoinProduct, url string) {
 	utils.GetTitle(c, &product.Title)
 	utils.GetDescription(c, &product.Description)
 	utils.GetPrice(c, &product.OriginalPrice)
@@ -98,32 +94,31 @@ func getProductInfo(c *colly.Collector, product *CoinProduct, url string) {
 	c.Visit(url)
 }
 
-// hasProductChanged checks if the product has changed compared to product in cache.
+// hasProductChanged checks if the product has changed compared to product in database.
 // Return true if it has changed. False otherwise
-func hasProductChanged(product *CoinProduct) bool {
+func (CoinProductServiceImpl) hasProductChanged(product *CoinProduct, db bitcask.Bitcask) bool {
 	// read from file
-	fileProduct, found := storage.Get("product_title")
+	// fileProduct, found := storage.Get("product_title")
 	// if no product in cache, product has changed
-	if !found {
-		return true
-	}
+	// if !found {
+	// return true
+	// }
 
-	// if current product is the same as product in file, then exit
-	if product.Title == fileProduct {
-		// log.Println(fmt.Sprintf("Product %s has not changed", product.Title))
-		return false
-	}
+	// // if current product is the same as product in file, then exit
+	// if product.Title == fileProduct {
+	// // log.Println(fmt.Sprintf("Product %s has not changed", product.Title))
+	// return false
+	// }
 	return true
 }
 
 // isCoinProduct check if the product is a coin product.
-// Return true if it is a coin product. False other wise
+// Return true if it is a coin product. False otherwise
 func isCoinProduct(product *CoinProduct) bool {
 	// check if product description contains "coin"
 	if strings.Contains(strings.ToLower(product.Description), "coin ") ||
 		strings.Contains(strings.ToLower(product.Title), "coin ") ||
 		strings.Contains(strings.ToLower(product.Description), "coins ") {
-		log.Println("Coin product found")
 		return true
 	}
 	return false
--- server/coin_products/coin_product_test.go
+++ server/coin_products/coin_product_test.go
@@ -1,6 +1,7 @@
 package check_coin_product
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gocolly/colly"
@@ -8,32 +9,77 @@ import (
 
 var c *colly.Collector
 
-// TestIsCoinProduct tests if it is able to detect if the product is a coin product
-func TestIsCoinProduct(t *testing.T) {
-	// A valid coin product
-	coinProduct := CoinProduct{Title: "Coin fjdsljf",
-		Description:   "jfldsjf coin",
-		OriginalPrice: 0.00}
-	shouldBeCoinProduct := isCoinProduct(&coinProduct)
-	if !shouldBeCoinProduct {
-		t.Errorf("Coin product not detected")
-	}
+// setup creates and returns an instance of the CoinProductServiceImpl struct
+func setup() CoinProductService {
+	var p CoinProductService
+	p = CoinProductServiceImpl{}
+	return p
+}
 
-	// create a non coin product
-	notCoinProduct := CoinProduct{Title: "jfsdjfsljf",
-		Description:   "jfldsfjf jjfdsl",
-		OriginalPrice: 0.00}
-
-	invalidCoinProduct := isCoinProduct(&notCoinProduct)
-
-	if invalidCoinProduct == true {
-		t.Errorf("false Coin product detected")
+// TestMakeDB tests the makeDB function is able to create a local database
+func TestMakeDB(t *testing.T) {
+	p := setup()
+	db := p.makeDB()
+	defer db.Close()
+	if db == nil {
+		t.Error("makeDB returned nil")
 	}
 }
 
-// TestCheck tests if it is able to check if the product is a coin product
-func TestCheck(t *testing.T) {
-	product := Check("https://www.penguinmagic.com/p/1806")
+// // create a non coin product
+// notCoinProduct := CoinProductResponse{Title: "jfsdjfsljf",
+// Description:   "jfldsfjf jjfdsl",
+// OriginalPrice: 0.00}
+
+// invalidCoinProduct := isCoinProduct(&notCoinProduct)
+
+// if invalidCoinProduct == true {
+// t.Errorf("false Coin product detected")
+// }
+// }
+
+func TestIsCoinProduct(t *testing.T) {
+	// valid product
+	product := CoinProduct{Title: "Coin fjdsljf"}
+	valid := isCoinProduct(&product)
+	if !valid {
+		t.Errorf("Product should be valid, instead got %s", product.Reason)
+	}
+
+	// invalid product
+	product = CoinProduct{Title: "jfsdjfsljf"}
+	valid = isCoinProduct(&product)
+	if valid {
+		t.Errorf("Product should be invalid, instead got %s", product.Reason)
+	}
+}
+
+// TestCheckInvalidProduct tests if it is able to detect an invalid product
+func TestCheckInvalidProduct(t *testing.T) {
+	p := setup()
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.penguinmagic.com", "www.penguinmagic.com/p/8474"),
+	)
+
+	product := CoinProduct{}
+	p.getProductInfo(c, &product, "https://www.penguinmagic.com/p/8474")
+	p.Check(&product)
+	if product.IsValid {
+		fmt.Println(fmt.Sprintf("TestCheckInvalidProduct product.IsValid: %v", product.IsValid)) // __AUTO_GENERATED_PRINT_VAR__
+		t.Errorf("Product should be invalid, instead got %s", product.Reason)
+	}
+}
+
+// TestCheckInvalidProduct tests if it is able to detect a valid product
+func TestCheckValidProduct(t *testing.T) {
+	p := setup()
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.penguinmagic.com", "www.penguinmagic.com/p/1797"),
+	)
+
+	product := CoinProduct{}
+	p.getProductInfo(c, &product, "https://www.penguinmagic.com/p/1797")
+	p.Check(&product)
 	if !product.IsValid {
 		t.Errorf("Product should be valid, instead got %s", product.Reason)
 	}
