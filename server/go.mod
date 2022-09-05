7621d8e36d74faab9835f31d704bbca46a964b83
shawn yu <shawn.yu5@hotmail.com>
2022-09-05 15:32:42 -0400

    refactor(server): test uses ginkgo

--- server/go.mod
+++ server/go.mod
@@ -17,14 +17,19 @@ require (
 	github.com/antchfx/htmlquery v1.2.4 // indirect
 	github.com/antchfx/xmlquery v1.3.10 // indirect
 	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gofrs/flock v0.8.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/onsi/ginkgo/v2 v2.1.6 // indirect
+	github.com/onsi/gomega v1.20.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/plar/go-adaptive-radix-tree v1.0.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
@@ -36,10 +41,12 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/exp v0.0.0-20200228211341-fcea875c7e85 // indirect
-	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/server_test.go
+++ server/server_test.go
@@ -2,86 +2,85 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	"testing"
 
 	"server/utils"
 
 	"github.com/gocolly/colly"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-// TestLoggerHandler tests the logger handler, and checks if the product obj we get back is correct
-func TestLoggerHandler(t *testing.T) {
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.penguinmagic.com", "www.penguinmagic.com/openbox/"),
-	)
+var _ = Describe("Server", func() {
+	Context("Logger Handler", func() {
+		It("Should return the same product as the one on penguin magic currently", func() {
+			c := colly.NewCollector(
+				colly.AllowedDomains("www.penguinmagic.com", "www.penguinmagic.com/openbox/"),
+			)
 
-	// the product we are expecting to get back
-	penguinProduct := LoggerProduct{} // product on penguinmagic
+			// the product we are expecting to get back
+			penguinProduct := LoggerProduct{} // product on penguinmagic
 
-	utils.GetTitle(c, &penguinProduct.Title)
-	utils.GetPrice(c, &penguinProduct.Original_price)
-	utils.GetDiscountedPrice(c, &penguinProduct.Discount_price)
-	utils.GetDiscountPercentage(c, &penguinProduct.Discount_percentage)
+			utils.GetTitle(c, &penguinProduct.Title)
+			utils.GetPrice(c, &penguinProduct.Original_price)
+			utils.GetDiscountedPrice(c, &penguinProduct.Discount_price)
+			utils.GetDiscountPercentage(c, &penguinProduct.Discount_percentage)
 
-	c.Visit("https://www.penguinmagic.com/openbox/")
+			c.Visit("https://www.penguinmagic.com/openbox/")
 
-	req := httptest.NewRequest(http.MethodGet, "/logger", nil)
-	w := httptest.NewRecorder()
-	loggerHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+			req := httptest.NewRequest(http.MethodGet, "/logger", nil)
+			w := httptest.NewRecorder()
+			loggerHandler(w, req)
+			res := w.Result()
+			defer res.Body.Close()
 
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("Error reading body: %v", err)
-	}
+			data, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				fmt.Println(fmt.Errorf("Error reading body: %v", err))
+			}
 
-	var loggerProduct LoggerProduct
-	err = json.Unmarshal(data, &loggerProduct) // parse json data into struct
-	if err != nil {
-		t.Errorf("Error unmarshalling json: %v", err)
-	}
+			var loggerProduct LoggerProduct
+			err = json.Unmarshal(data, &loggerProduct) // parse json data into struct
+			if err != nil {
+				fmt.Println(fmt.Errorf("Error unmarshalling json: %v", err))
+			}
 
-	// the product our logger handler returns should be the same as the product we got from penguinmagic
-	if loggerProduct != penguinProduct {
-		t.Errorf("Expected %v, got %v", loggerProduct, data)
-	}
-}
+			// the product our logger handler returns should be the same as the product we got from penguinmagic
+			Expect(loggerProduct).To(Equal(penguinProduct))
+		})
 
-// TestSearchHandlerSuccess tests the search handler with a valid query
-func TestSearchHandlerSuccess(t *testing.T) {
-	// a successful search should return a list of products
-	req := httptest.NewRequest(http.MethodPost, "/search", strings.NewReader("hello"))
-	w := httptest.NewRecorder()
-	searchHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+		Context("Search Handler", func() {
+			It("Should return a list of products", func() {
+				// a successful search should return a list of products
+				req := httptest.NewRequest(http.MethodPost, "/search", strings.NewReader("hello"))
+				w := httptest.NewRecorder()
+				searchHandler(w, req)
+				res := w.Result()
+				defer res.Body.Close()
 
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
+				data, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					panic(err)
+				}
 
-	// if we didnt get anything back, something has gone wrong
-	if len(data) == 0 {
-		t.Errorf("Expected data, got %v", data)
-	}
+				// if we didnt get anything back, something has gone wrong
+				Expect(len(data)).ToNot(Equal(0), "Expected data to not be empty")
 
-}
+			})
 
-// TestSearchHandlerFail tests the search handler with a invalid request method
-func TestSearchHandlerFail(t *testing.T) {
-	// a successful search should return a list of products
-	req := httptest.NewRequest(http.MethodGet, "/search", nil)
-	w := httptest.NewRecorder()
-	searchHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	if res.StatusCode != 404 {
-		t.Errorf("Expected status code 404, got %v", res.StatusCode)
-	}
-}
+			It("Should return 404 given an invalid request method", func() {
+				// a successful search should return a list of products
+				req := httptest.NewRequest(http.MethodGet, "/search", nil)
+				w := httptest.NewRecorder()
+				searchHandler(w, req)
+				res := w.Result()
+				defer res.Body.Close()
+				Expect(res.StatusCode).To(Equal(404), "Expected status code 404, got %v", res.StatusCode)
+			})
+		})
+	})
+})
