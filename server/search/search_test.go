3b31fa88e438c572acf5f94fcae8793a44633b93
shawn yu <shawn.yu5@hotmail.com>
2022-09-05 18:35:56 -0400

    test(search): uses ginkgo

--- server/search/search_test.go
+++ server/search/search_test.go
@@ -4,44 +4,36 @@ import (
 	"context"
 	"log"
 	"os"
-	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-// beforeEach called before each unit test, sets up the service with nessary middleware
-// returns an instance of the service
-func beforeEach() SearchService {
-	logger := log.New(os.Stdout, "", log.LUTC)
-
+var _ = Describe("Search", func() {
 	var ss SearchService
-	ss = SearchServiceImpl{}
-	ss = LoggingMiddleware{Logger: logger, Next: ss}
-	return ss
-}
+	BeforeEach(func() {
+		logger := log.New(os.Stdout, "", log.LUTC)
+		ss = SearchServiceImpl{}
+		ss = LoggingMiddleware{Logger: logger, Next: ss}
+	})
 
-// TestAbleToConnectToDb tests if we can connect to the database
-func TestAbleToConnectToDb(t *testing.T) {
-	ss := beforeEach()
-	client := ss.connectDB()
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+	It("Should be able to connect to the database", func() {
+		client := ss.connectDB()
+		defer func() {
+			if err := client.Disconnect(context.TODO()); err != nil {
+				panic(err)
+			}
+		}()
 
-	if client == nil {
-		t.Error("Failed to connect to mongodb")
-	}
-}
+		Expect(client).NotTo(BeNil(), "Failed to connect to mongodb")
+	})
 
-func TestSearchByRegex(t *testing.T) {
-	ss := beforeEach()
-	product := &Product{Title: "card"}
-	found, err := ss.SearchByRegex(product)
-	if err != nil {
-		t.Error("Error searching for product")
-	}
-
-	if len(found) == 0 {
-		t.Error("Failed to find product")
-	}
-}
+	Context("SearchByRegex", func() {
+		It("Should be able to search for a product", func() {
+			product := &Product{Title: "card"}
+			found, err := ss.SearchByRegex(product)
+			Expect(err).To(BeNil(), "Error searching for product")
+			Expect(len(found)).NotTo(Equal(0), "Failed to find product")
+		})
+	})
+})
